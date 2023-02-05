6bead8cc68636a9515038d416130ca6466fdeac4
liudmyla-ch <110854340+liudmyla-ch@users.noreply.github.com>
2023-02-05 23:32:29 +0200

    add bad request

--- src/js/seach.js
+++ src/js/seach.js
@@ -1,7 +1,9 @@
-import NewAskServer from "./fetch-films";
+import NewAskServer from './fetch-films';
 import loader from './spinner';
 
 const seachServer = new NewAskServer();
+const header = document.querySelector('header');
+console.log(header);
 
 const STORAGE_KEY = 'genresId';
 const STORAGE_PAGE = 'storagePage';
@@ -10,59 +12,71 @@ let currentRequest = ''; // for pagination
 const form = document.querySelector('.search-form');
 const searchFormInput = document.querySelector('.search-form__input');
 const gallery = document.querySelector('.films__list');
+const badRequest = document.querySelector('.bad-request')
 
 form.addEventListener('submit', formHandler);
 
 async function formHandler(event) {
-    event.preventDefault();
-    loader.spinner.show()
+  event.preventDefault();
+  badRequest.innerHTML = ""
+  
 
-    const request = searchFormInput.value.trim();
-    if (!request) {
-        return;
-    }
+  const request = searchFormInput.value.trim();
+  if (!request) {
+    return;
+  }
+  loader.spinner.show();
 
-    const response = await seachServer.fetchSearchId(request);
+  const response = await seachServer.fetchSearchId(request);
+  
 
-    if (response.length > 0) {
-        currentRequest = request; // for pagination
-        localStorage.setItem(STORAGE_PAGE, JSON.stringify(response));
-        drawGallery();
-    } else {
-        console.log('Search result not successful. Enter the correct movie name and try again.');
-        // error message generation must be here
-    }
-    loader.spinner.close()
+  if (response.length > 0) {
+    currentRequest = request; // for pagination
+    localStorage.setItem(STORAGE_PAGE, JSON.stringify(response));
+    drawGallery();
+  } else {
+    loader.spinner.close();
+    badRequest.innerHTML = 'Search result not successful. Enter the correct movie name and try again.';
+    console.log(
+      'Search result not successful. Enter the correct movie name and try again.'
+    );
+    // error message generation must be here
+  }
+  loader.spinner.close();
 }
 
 function drawGallery() {
-    const IMG_URL = `https://image.tmdb.org/t/p/w500/`;
-    const allGenresArr = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    const filmList = JSON.parse(localStorage.getItem(STORAGE_PAGE));
+  const IMG_URL = `https://image.tmdb.org/t/p/w500/`;
+  const allGenresArr = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const filmList = JSON.parse(localStorage.getItem(STORAGE_PAGE));
 
-    let markup = '';
+  let markup = '';
 
-    filmList.forEach(film => {
-        let genres = [];
+  filmList.forEach(film => {
+    let genres = [];
 
-        film.genre_ids.forEach(genreId => {
-            const currentGenre = allGenresArr.filter(elem => elem.id === genreId);
-            genres.push(currentGenre[0].name);
-        })
+    film.genre_ids.forEach(genreId => {
+      const currentGenre = allGenresArr.filter(elem => elem.id === genreId);
+      genres.push(currentGenre[0].name);
+    });
 
-        markup = markup + `
+    markup =
+      markup +
+      `
         <li class="films__card">
-            <img class="films__img" src="${IMG_URL}${film.poster_path}" alt="${film.title}" loading="lazy" />
+            <img class="films__img" src="${IMG_URL}${film.poster_path}" alt="${
+        film.title
+      }" loading="lazy" />
             <div class="films__desc">
                 <h3 class="films__title">${film.title}</h3>
                 <p class="films__genre">
-                    ${genres.slice(0, 2).join(", ")}
+                    ${genres.slice(0, 2).join(', ')}
                     <span>|</span>
                     ${film.release_date.slice(0, 4)}
                 </p>
             </div>
-        </li>`
-    });
+        </li>`;
+  });
 
-    gallery.innerHTML = markup;
+  gallery.innerHTML = markup;
 }
