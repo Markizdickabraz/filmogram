3cbc3b74592bd510e4332a8b518fc4d24bb9581f
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-02-06 09:16:27 +0200

    current film id in session storage

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import './js/home';
 import './js/library';
 import './js/modal/film-modal';
 import './js/modal/team-modal';
-import './js/pagination'
+import './js/pagination';
 import './js/seach';
+import './js/local-strage';
+import './js/add-to-watched&queue';
--- src/js/modal/film-modal.js
+++ src/js/modal/film-modal.js
@@ -2,32 +2,35 @@ const filmsList = document.querySelector('.films__list');
 const modal = document.querySelector('.renderModal');
 const STORAGE_PAGE = 'storagePage';
 const STORAGE_KEY = 'genresId';
+
 const backdrop = document.querySelector('.backdrop');
 
 const IMGURL = `https://image.tmdb.org/t/p/w500/`;
 let filmArr = [];
-
 filmsList.addEventListener('click', modalOpen);
-
 function modalOpen(e) {
   // if (e.target.className !== "films__card") {
   //     return;
   CloseModalClickEsc();
   backdrop.classList.remove('isHidden');
   const getFilmsJson = localStorage.getItem(STORAGE_PAGE);
+
   const parseFilmsJson = JSON.parse(getFilmsJson);
   for (let i = 0; i < parseFilmsJson.length; i += 1) {
-    parseFilmsJson[i]
-    if (parseFilmsJson[i].title === e.target.alt
-      || parseFilmsJson[i].title === e.target.textContent
-      || parseFilmsJson[i].title === e.target.parentElement.children[0].textContent) {
+    parseFilmsJson[i];
+    if (
+      parseFilmsJson[i].title === e.target.alt ||
+      parseFilmsJson[i].title === e.target.textContent ||
+      parseFilmsJson[i].title === e.target.parentElement.children[0].textContent
+    ) {
       filmArr.push(parseFilmsJson[i]);
       const getGenresJson = localStorage.getItem(STORAGE_KEY);
       const parseGenresJson = JSON.parse(getGenresJson);
-
       const markupModal = filmArr
+
         .map(
           ({
+            id,
             poster_path,
             vote_average,
             vote_count,
@@ -36,13 +39,15 @@ function modalOpen(e) {
             genre_ids,
             overview,
           }) => {
-              let genreArr = [];
-      for (const genre of parseGenresJson) {
-        if (genre_ids.includes(genre.id)) {
-          genreArr.push(genre.name);
-        }
-      }
-            return `<div class="film-modal">
+            let genreArr = [];
+            for (const genre of parseGenresJson) {
+              if (genre_ids.includes(genre.id)) {
+                genreArr.push(genre.name);
+              }
+            }
+            return (
+              sessionStorage.setItem('current-film-id', id),
+              `<div class="film-modal" >
   <button
     class="film-modal__btn-icon"
     data-modal-close-p
@@ -57,13 +62,15 @@ function modalOpen(e) {
     <img class="film-modal__img" src="${IMGURL}${poster_path}" alt="${title}" />
   </div>
   <div class="film-modal__info-container">
-    <h2 class="film-modal__title">${title}</h2>
+    <h2 class="film-modal__title">${title},</h2>
     <table class="film-modal__table">
       <tbody class="film-modal__cell1">
         <tr>
           <td class="film-modal__cell film-modal__modal-text">Vote / Votes</td>
           <td class="film-modal__cell film-modal__modal-text">
-            <span class="film-modal__span film-modal__span-vote">${vote_average.toFixed(1)}</span> /
+            <span class="film-modal__span film-modal__span-vote">${vote_average.toFixed(
+              1
+            )}</span> /
             <span class="film-modal__span film-modal__span--votes">${vote_count}</span>
           </td>
         </tr>
@@ -87,7 +94,7 @@ function modalOpen(e) {
         <tr>
           <td class="film-modal__cell film-modal__modal-text">Genre</td>
           <td class="film-modal__cell film-modal__modal-text--genre">
-            ${genreArr.join(", ")}
+            ${genreArr.join(', ')}
           </td>
         </tr>
       </tbody>
@@ -98,7 +105,7 @@ function modalOpen(e) {
 
     <ul class="film-modal__btn-list">
       <li class="film-modal__btn-item">
-        <button class="btn modal__btn" type="" data-action="watch">
+        <button class="btn modal__btn" type="button" data-action="watch">
           ADD TO WATCHED
         </button>
       </li>
@@ -109,7 +116,8 @@ function modalOpen(e) {
       </li>
     </ul>
   </div>
-</div>`;
+</div>`
+            );
           }
         )
         .join('');
@@ -119,23 +127,20 @@ function modalOpen(e) {
 }
 
 backdrop.addEventListener('click', closeModal);
-
 function closeModal(e) {
   if (e.target.classList[0] !== 'backdrop') {
+    return;
+  }
+  backdrop.classList.add('isHidden');
+  filmArr = [];
+}
+
+function CloseModalClickEsc() {
+  document.addEventListener('keydown', event => {
+    if (event.key !== 'Escape') {
       return;
     }
     backdrop.classList.add('isHidden');
     filmArr = [];
-  }
-
-
-function CloseModalClickEsc() {
-  document.addEventListener('keydown', event => {
-  
-  if (event.key !== 'Escape') {
-    return;
-  }
-    backdrop.classList.add('isHidden');
-    filmArr = [];
-})
+  });
 }
