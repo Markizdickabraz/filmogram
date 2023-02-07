b80ff9ccc6394456931e6fa4fb34049c8cb5fd24
Angori4 <110497521+Angori4@users.noreply.github.com>
2023-02-07 23:43:06 +0200

    btn_up

---
+++ src/js/btn-upp.js
@@ -0,0 +1,18 @@
+const upBtn = refs.btnToTop;
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction () {
+    if(window.scrollY > 300 && !refs.body.classList.contains('modal-open')) {
+        upBtn.style.display = "block";
+    } else {
+        upBtn.style.display = "none";
+}
+};
+upBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+});
+
