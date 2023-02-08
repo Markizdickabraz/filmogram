7ceb87557740fdd95e41ec7d8e3b49d32678ca60
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2023-02-08 13:33:55 +0200

    Revert "library v1.1"

--- src/js/library.js
+++ src/js/library.js
@@ -1,4 +1,4 @@
-import modalOpen from './modal/film-modal';
+import modalOpen from "./modal/film-modal";
 
 const STORAGE_PAGE = 'storagePage';
 const STORAGE_KEY = 'genresId';
@@ -6,47 +6,27 @@ const STORAGE_KEY = 'genresId';
 const watchBtn = document.querySelector('.js-btn__watched');
 const queueBtn = document.querySelector('.js-btn__queue');
 const libraryList = document.querySelector('.films-library__list');
-renderPageWached();
-queueBtn.addEventListener('click', e => {
-  // e.preventDefault();
 
-  watchBtn.classList.remove('isActive') ||
-    queueBtn.classList.add('isActive') ||
-    renderPageQueue();
-});
-watchBtn.addEventListener('click', e => {
-  // e.preventDefault();
-  renderPageWached();
-  if (watchBtn.classList[3] !== 'isActive')
-    watchBtn.classList.add('isActive') ||
-      queueBtn.classList.remove('isActive' || renderPageWached());
-});
+renderWatchedList();
 
-// renderLibrary();
-// console.dir(watchBtn);
-// console.dir(queueBtn);
-// function renderLibrary() {
-//   if (watchBtn.classList[3] === 'isActive') {
-//     console.dir(watchBtn);
-//     renderPageWached();
-//   }
-// }
+function renderWatchedList() {
+  if (watchBtn) {
+    if (watchBtn.classList[2] === 'isActive') {
+      console.dir(watchBtn);
+      renderPage();
+    }
+  }
+};
 
-// if (queueBtn.classList[3] === 'isActive') {
-//   renderPageQueue();
-//   console.dir(queueBtn);
-// }
-
-function renderPageWached() {
-  const watchPageJson = localStorage.getItem('watched');
-  const parseWatched = JSON.parse(watchPageJson);
-  console.log(watchPageJson);
-  const getGenresJson = localStorage.getItem(STORAGE_KEY);
-  const parseGenresJson = JSON.parse(getGenresJson);
-  const IMGURL = `https://image.tmdb.org/t/p/w500/`;
-  const notFound = `https://i.scdn.co/image/ab67616d0000b273d9495d198c584e0e64f3ad9d`;
-  const markupPage = parseWatched
-    .map(({ id, poster_path, title, genre_ids, release_date }) => {
+function renderPage() {
+    const watchPageJson = localStorage.getItem(STORAGE_PAGE);
+    const parseWatched = JSON.parse(watchPageJson);
+    console.log(parseWatched);
+    const getGenresJson = localStorage.getItem(STORAGE_KEY);
+    const parseGenresJson = JSON.parse(getGenresJson);
+    const IMGURL = `https://image.tmdb.org/t/p/w500/`;
+    const notFound = `https://i.scdn.co/image/ab67616d0000b273d9495d198c584e0e64f3ad9d`;
+    const markupPage = parseWatched.map(({id, poster_path, title, genre_ids, release_date}) => {
       let genreArr = [];
       for (const genre of parseGenresJson) {
         if (genre_ids.includes(genre.id)) {
@@ -60,41 +40,7 @@ function renderPageWached() {
   <div class="films__desc">
     <h3 class="films__title">${title}</h3>
     <p class="films__genre">
-    ${genreArr.slice(0, 2).join(', ')}
-      <span>|</span>
-      ${release_date.slice(0, 4)}
-    </p>
-  </div>
-</li>
-            `;
-    })
-    .join('');
-  libraryList.innerHTML = markupPage;
-}
-function renderPageQueue() {
-  const queuePageJson = localStorage.getItem('queue');
-  const parseQueue = JSON.parse(queuePageJson);
-  console.log(queuePageJson);
-  const getGenresJson = localStorage.getItem(STORAGE_KEY);
-  const parseGenresJson = JSON.parse(getGenresJson);
-  const IMGURL = `https://image.tmdb.org/t/p/w500/`;
-  const notFound = `https://i.scdn.co/image/ab67616d0000b273d9495d198c584e0e64f3ad9d`;
-  const markupPage = parseQueue
-    .map(({ id, poster_path, title, genre_ids, release_date }) => {
-      let genreArr = [];
-      for (const genre of parseGenresJson) {
-        if (genre_ids.includes(genre.id)) {
-          genreArr.push(genre.name);
-        }
-      }
-      return `
-            <li class="films__card" data-id="${id}" id="film_card">
-<img class="films__img" src="${IMGURL}${poster_path}" alt="${title}" loading="lazy" onerror="this.onerror=null; this.src='${notFound}';";"
-/>
-  <div class="films__desc">
-    <h3 class="films__title">${title}</h3>
-    <p class="films__genre">
-    ${genreArr.slice(0, 2).join(', ')}
+    ${genreArr.slice(0, 2).join(", ")}
       <span>|</span>
       ${release_date.slice(0, 4)}
     </p>
