636e83a9a8d8d396be5d326ecd31c94a48465a60
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-02-08 17:27:11 +0200

    library v1.4

--- src/js/library.js
+++ src/js/library.js
@@ -1,4 +1,4 @@
-import modalOpen from "./modal/film-modal";
+import modalOpen from './modal/film-modal';
 
 const STORAGE_PAGE = 'storagePage';
 const STORAGE_KEY = 'genresId';
@@ -6,19 +6,32 @@ const STORAGE_KEY = 'genresId';
 const watchBtn = document.querySelector('.js-btn__watched');
 const queueBtn = document.querySelector('.js-btn__queue');
 const libraryList = document.querySelector('.films-library__list');
+
 queueBtn.addEventListener('click', e => {
   // e.preventDefault();
-=======
-renderWatchedList();
 
-function renderWatchedList() {
-  if (watchBtn) {
-    if (watchBtn.classList[2] === 'isActive') {
-      console.dir(watchBtn);
-      renderPage();
-    }
-  }
-};
+  watchBtn.classList.remove('isActive') ||
+    queueBtn.classList.add('isActive') ||
+    renderPageQueue();
+});
+watchBtn.addEventListener('click', e => {
+  // e.preventDefault();
+  renderPageWached();
+  if (watchBtn.classList[3] !== 'isActive')
+    watchBtn.classList.add('isActive') ||
+      queueBtn.classList.remove('isActive' || renderPageWached());
+});
+
+// renderLibrary();
+// console.dir(watchBtn);
+// console.dir(queueBtn);
+// function renderLibrary() {
+//   if (watchBtn.classList[3] === 'isActive') {
+//     console.dir(watchBtn);
+//     renderPageWached();
+//   }
+// }
+
 // if (queueBtn.classList[3] === 'isActive') {
 //   renderPageQueue();
 //   console.dir(queueBtn);
@@ -34,17 +47,7 @@ export function renderPageWached() {
   const notFound = `https://i.scdn.co/image/ab67616d0000b273d9495d198c584e0e64f3ad9d`;
   const markupPage = parseWatched
     .map(({ id, poster_path, title, genre_ids, release_date }) => {
-=======
-function renderPage() {
-    const watchPageJson = localStorage.getItem(STORAGE_PAGE);
-    const parseWatched = JSON.parse(watchPageJson);
-    console.log(parseWatched);
-    const getGenresJson = localStorage.getItem(STORAGE_KEY);
-    const parseGenresJson = JSON.parse(getGenresJson);
-    const IMGURL = `https://image.tmdb.org/t/p/w500/`;
-    const notFound = `https://i.scdn.co/image/ab67616d0000b273d9495d198c584e0e64f3ad9d`;
-    const markupPage = parseWatched.map(({id, poster_path, title, genre_ids, release_date}) => {      
-         let genreArr = [];
+      let genreArr = [];
       for (const genre of parseGenresJson) {
         if (genre_ids.includes(genre.id)) {
           genreArr.push(genre.name);
@@ -93,8 +96,7 @@ export function renderPageQueue() {
     <h3 class="films__title">${title}</h3>
     <p class="films__genre">
     ${genreArr.slice(0, 2).join(', ')}
-     ${genreArr.slice(0, 2).join(", ")}
-     <span>|</span>
+      <span>|</span>
       ${release_date.slice(0, 4)}
     </p>
   </div>
@@ -108,3 +110,4 @@ export function renderPageQueue() {
 if (libraryList) {
   libraryList.addEventListener('click', modalOpen);
 }
+// asda
